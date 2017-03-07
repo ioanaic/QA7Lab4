31a87502cf4488f9e5a2b7228e20944f1e26d114
FirstJavaProj <crisan_ioana2005@yahoo.com>
2017-03-07 19:58:35 +0200

    Add lab 5 Vechicle

---
+++ src/lab5/oop/Vehicle.java
@@ -0,0 +1,15 @@
+package lab5.oop;
+
+/**
+ * Created by Ioana on 3/7/2017.
+ */
+public class Vehicle {
+
+    public void moves(){
+        System.out.println("Vechicle moves");
+    }
+
+    public void stops(){
+        System.out.println("Vehicle stops!");
+    }
+}
